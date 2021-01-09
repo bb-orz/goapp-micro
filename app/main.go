7e7635854608b2cfcdc9ef146e9b94f2f2cd114d
gofuncchan <gofuncchan@163.com>
2021-01-09 15:46:34 +0800

    add builder file

--- app/main.go
+++ app/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/bb-orz/goinfras"
+	"github.com/bb-orz/goinfras/XGlobal"
+	"github.com/bb-orz/goinfras/XLogger"
+	"github.com/bb-orz/goinfras/XValidate"
+	"github.com/spf13/viper"
+	"goapp/starter/exampleMicro"
 )
 
 
@@ -21,4 +26,21 @@ func main() {
 	// 运行应用,启动已注册的资源组件
 	fmt.Println("Application Starting ......")
 	app.Up()
+}
+
+// 注册应用组件启动器，把基础设施各资源组件化
+func RegisterStarter(viperCfg *viper.Viper) {
+	goinfras.RegisterStarter(XGlobal.NewStarter())
+
+	goinfras.RegisterStarter(XLogger.NewStarter())
+
+	// 注册验证器
+	goinfras.RegisterStarter(XValidate.NewStarter())
+
+	// Register RPC Service Server
+	goinfras.RegisterStarter(exampleMicro.NewStarter())
+
+	// 对资源组件启动器进行排序
+	goinfras.SortStarters()
+
 }
